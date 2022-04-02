e674dcecaf01d3f9e0736c1f4194c9bd5db853d1
Danyil Somin <danya1203s@gmail.com>
2022-04-02 23:14:12 +0300

    in progress

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,12 @@ int bubbleSortTest()
     {
         for (int j = i + 1; j < LEN; ++j)
         {
-        
+            if (arr[j] < arr[j - 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
         }
     }
 }
