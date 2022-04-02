ff949ee000bba76ca30b2158da6eff39ba563091
Danyil Somin <danya1203s@gmail.com>
2022-04-02 20:03:37 +0300

    in progress

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,24 @@
 using namespace emscripten;
 
 #include <cctype>
+#include <chrono>
+#include <cstdlib>
 
 uint32_t getInt() {
     return 100;
+}
+
+int bubbleSortTest()
+{
+    static constexpr int LEN = 10'000;
+    int arr[LEN];
+    for (int i = 0; i < LEN; ++i)
+    {
+        for (int j = i + 1; j < LEN; ++j)
+        {
+        
+        }
+    }
 }
 
 EMSCRIPTEN_BINDINGS(diploma_module) {
