ad51a297fc760841b2e5629e29762e5aa92f0671
Danyil Somin <danya1203s@gmail.com>
2022-04-09 14:21:17 +0300

    Move scripts outside src

---
+++ web.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+source /home/somin/emsdk/emsdk_env.sh
+
+emcc -lembind \
+     -o ./src/index.js \
+     ./src/index.cpp \
+     ./src/bible.cpp \
+     ./src/tests.cpp \
+     ./src/md5.cpp \
+     -sMODULARIZE \
+     -s 'EXPORT_NAME="createWasmBench"' \
+     -O3
+
+python3 -m http.server 8888 --bind 0.0.0.0 --directory ./src
